aa53daeebb4840ad6d4a649370f3e9faae9079ba
Jacques <jacdup01@gmail.com>
2020-05-05 10:07:03 +0200

    Fix direction problem in FO circ MBF on endcaps

--- src/Jacques/plotNodeCurrent.m
+++ src/Jacques/plotNodeCurrent.m
@@ -5,13 +5,16 @@ for node = 1:numNodes
     iter = iter + 1;
     node_to_plot = 4;
     dofs = 1:2:length(DOF_mat1);
-    dofs_circ = 1:4:length(DOF_mat);
+    dofs_circ = 1:2:length(DOF_mat);
 %     x_axis = 0:360/vertices:359;
     x_axis = linspace(0,360,length(dofs));
     x_axis_circ = linspace(0,360,length(dofs_circ));
 
     current_norm(:,iter) = abs(I_vec_norm(DOF_mat1(dofs,node))+I_vec_norm(DOF_mat2(dofs,node))+I_vec_norm(DOF_mat3(dofs,node)));
     current_MBF(:,iter)  = abs(I_vec(DOF_mat1(dofs,node))+I_vec(DOF_mat2(dofs,node))+I_vec(DOF_mat3(dofs,node)));
+    
+%      current_norm(:,iter) = abs(I_vec_norm(DOF_mat1(dofs,node)));
+%     current_MBF(:,iter)  = abs(I_vec(DOF_mat1(dofs,node)));
     
     current_norm_circ(:,iter) = abs(I_vec_norm(DOF_mat(dofs_circ,node)));
     current_MBF_circ(:,iter) = abs(I_vec(DOF_mat(dofs_circ,node)));
@@ -25,7 +28,10 @@ end
 %         hold on
 %         plot(x_axis_circ,current_MBF_circ);
 %         hold off
-surf(x_axis_circ, linspace(1,2,numNodes), (current_norm_circ)');
+surf(x_axis, linspace(1,2,numNodes), (current_norm - current_MBF)');
+xlabel('Circumference (\phi)');
+ylabel('Cylinder length (m)');
+zlabel('Current magnitude');
 % hold on
 % surf(x_axis, 1:numNodes, current_MBF');
 % hold off
