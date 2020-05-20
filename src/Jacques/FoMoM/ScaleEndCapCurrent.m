aa6141b39c613a1bdc51dfc48b723837a6411363
Jacques <jacdup01@gmail.com>
2020-05-20 13:36:54 +0200

    Fix MBF_Circ for arbitrary cyl endcap spec

--- src/Jacques/FoMoM/ScaleEndCapCurrent.m
+++ src/Jacques/FoMoM/ScaleEndCapCurrent.m
@@ -1,17 +1,21 @@
-function [I_vec] = ScaleEndCapCurrent(DOF_mat1, DOF_mat,theta1, theta2, I_vec)
+function [I_vec] = ScaleEndCapCurrent(DOF_mat1, DOF_mat,theta1, theta2, I_vec, cyl_def)
 
 
-first_endcap_axial       = DOF_mat1(:,1);
-second_endcap_axial      = DOF_mat1(:,end);
+if (cyl_def.firstNode == "endCap")
+    first_endcap_axial       = DOF_mat1(:,1);
+    first_endcap_circ        = nonzeros(DOF_mat(:,end-1));
+    currents_endcap1         = I_vec(first_endcap_axial);
+    current_maxVal_endcap1   = max(currents_endcap1);
+    I_vec(first_endcap_circ(2:2:end)) = dot(I_vec(first_endcap_circ(2:2:end)),repelem((current_maxVal_endcap1), length(first_endcap_circ)/2)); % So that all values are smaller than maxVal
+end
 
-first_endcap_circ        = nonzeros(DOF_mat(:,end-1));
-second_endcap_circ       = nonzeros(DOF_mat(:,end));
-
-currents_endcap1         = I_vec(first_endcap_axial);
-current_maxVal_endcap1   = max(currents_endcap1);
-
-currents_endcap2         = I_vec(second_endcap_axial);
-current_maxVal_endcap2   = max(currents_endcap2);
+if cyl_def.lastNode == "endCap"
+    second_endcap_axial      = DOF_mat1(:,end);
+    second_endcap_circ       = nonzeros(DOF_mat(:,end));
+    currents_endcap2         = I_vec(second_endcap_axial);
+    current_maxVal_endcap2   = max(currents_endcap2);
+    I_vec(second_endcap_circ(2:2:end)) = dot(I_vec(second_endcap_circ(2:2:end)),repelem((current_maxVal_endcap2), length(second_endcap_circ)/2));
+end
 
 % I_vec(first_endcap_circ) = I_vec(first_endcap_circ)*real(current_maxVal_endcap1); % So that all values are smaller than maxVal
 % I_vec(second_endcap_circ) = I_vec(second_endcap_circ)*real(current_maxVal_endcap2);
@@ -21,6 +25,6 @@ current_maxVal_endcap2   = max(currents_endcap2);
 % I_vec(first_endcap_circ(2:2:end)) = I_vec(first_endcap_circ(2:2:end))*real(current_maxVal_endcap1); % So that all values are smaller than maxVal
 % I_vec(second_endcap_circ(2:2:end)) = I_vec(second_endcap_circ(2:2:end))*real(current_maxVal_endcap2);
 
-I_vec(first_endcap_circ(2:2:end)) = dot(I_vec(first_endcap_circ(2:2:end)),repelem((current_maxVal_endcap1), length(first_endcap_circ)/2)); % So that all values are smaller than maxVal
-I_vec(second_endcap_circ(2:2:end)) = dot(I_vec(second_endcap_circ(2:2:end)),repelem((current_maxVal_endcap2), length(first_endcap_circ)/2));
+
+
 end
